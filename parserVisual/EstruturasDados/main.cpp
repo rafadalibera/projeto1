27a8373f8c7c8e23f04f56421dfe09aaa67cd669
cicero <cicero@lasca.ic.unicamp.br>
2014-03-15 00:20:07 -0300

    Adicionando um projetinho para fazer e testar estruturas de dados

---
+++ parserVisual/EstruturasDados/main.cpp
@@ -0,0 +1,77 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+typedef struct{
+	char * Title;
+	char * Abstract;
+	char * Author;
+	char * Date;
+	char * Image;
+	char * Source;
+	char * Text;
+	int numCol;
+	short mascaraPropriedades[7];
+} Noticia;
+
+typedef struct {
+	int capacidade;
+	int tamanho;
+	Noticia * valores;
+}ListaNoticias;
+
+//Os argumentos precisam ser strings bem formadas (com NUL no final). Todos os objetos sao reinstanciados, ou seja, pode dar free nos fontes depois
+//Nao mudar abstrac para abstract. Palavra reservada.
+Noticia NewNoticia(char * title, char * abstrac, char * author, char * date, char * image, char * source, char * text, int numCol){
+	Noticia retorno;
+	
+	retorno.Abstract = (char*)calloc(strlen(abstrac) + 1, sizeof(char));
+	strcpy(retorno.Abstract, abstrac);
+
+	retorno.Author = (char*)calloc(strlen(author) + 1, sizeof(char));
+	strcpy(retorno.Author, author);
+
+	retorno.Date = (char*)calloc(strlen(date) + 1, sizeof(char));
+	strcpy(retorno.Date, date);
+
+	retorno.Image = (char*)calloc(strlen(image) + 1, sizeof(char*));
+	strcpy(retorno.Image, image);
+
+	retorno.Source = (char *)calloc(strlen(source) + 1, sizeof(char));
+	strcpy(retorno.Source, source);
+
+	retorno.Text = (char *)calloc(strlen(text), sizeof(char));
+	strcpy(retorno.Text, text);
+
+	retorno.numCol = numCol;
+}
+
+//Cria nova instancia de uma lista de noticias
+ListaNoticias NewListaNoticias(int capacidade){
+	ListaNoticias retorno;
+
+	retorno.capacidade = capacidade;
+	retorno.tamanho = 0;
+	retorno.valores = (Noticia *)calloc(capacidade, sizeof(Noticia));
+	
+	return retorno;
+}
+
+void AppendElemento(ListaNoticias * listaNoticias, Noticia novaNoticia){
+	if ((*listaNoticias).tamanho + 1 > (*listaNoticias).capacidade){
+		(*listaNoticias).capacidade = 2 * (*listaNoticias).capacidade;
+		(*listaNoticias).valores = (Noticia *)realloc((*listaNoticias).valores, (*listaNoticias).capacidade * sizeof(int));
+	}
+
+	(*listaNoticias).valores[(*listaNoticias).tamanho] = novaNoticia;
+	(*listaNoticias).tamanho++;
+}
+
+int main(){
+	ListaNoticias lista = NewListaNoticias(10);
+	Noticia not = NewNoticia("titulo", "abstract", "author", "data", "imagem", "fonte", "texto balbalablab", 10);
+
+	AppendElemento(&lista, not);
+
+}
